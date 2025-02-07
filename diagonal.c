f2ac525080c8f427fc9ca6887f5d24f5d606045f
annujames <annujames2028@cs.sjcetpalai.ac.in>
2025-02-07 10:41:02 +0530

    Add files via upload

---
+++ diagonal.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+  int main(){
+     int row,col,j,i;
+     printf("\n enter the rows and columns");
+     scanf("%d%d",&row,&col);
+     int matrix[row][col];
+     printf("\nenter the elements");
+     for(int i=0;i<row;i++){
+        for(j=0;j<col;j++){
+        scanf("%d",&matrix[i][j]);
+        }
+      }  
+     int sum=0;
+     for(int i=0;i<row;i++){
+        for(j=0;j<col;j++){
+            if (i==j){
+                sum=sum+matrix[i][j];
+            }
+         }
+      }
+     printf("the sum of diagonal elements is");
+     printf("%d",sum);
+     return 0;
+}
+
+
